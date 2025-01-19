06919f7d4bbdb7b22cc43805e18058d6f2715b9f
biodun-dev <abdultoheeb97@gmail.com>
2025-01-19 07:36:17 +0100

    Initial Commit

---
+++ HelloSwiftApp/HelloSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftAppApp.swift
+//  HelloSwiftApp
+//
+//  Created by User on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
